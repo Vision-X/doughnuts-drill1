dd21b61096bbfdbbd1db9c10a239227a05ffceb4
Mike Marlow <michael.s.marlow@gmail.com>
2017-12-04 15:27:24 -0700

    rearrage

--- client/public/src/main.js
+++ client/public/src/main.js
@@ -3,7 +3,7 @@ let add = document.getElementById('append')
 
 button.addEventListener('click', (e) => {
   add.innerText = ''
-  fetch('https://whispering-thicket-15412.herokuapp.com/')
+  fetch('https://arcane-eyrie-18906.herokuapp.com/')
   .then((res) => res.json())
   .then((data) => {
     data.forEach((item) => {
